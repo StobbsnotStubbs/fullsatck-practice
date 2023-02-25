4cd14bcf63fd66b22a50c9f0f0d9e80f8aaa6978
Kevin Stobbs <Kevin.Stobbs94@gmail.com>
2023-02-25 11:23:17 +0000

    started adding in mongoose and databases

--- server.js
+++ server.js
@@ -3,6 +3,24 @@
 const express = require("express"); // npm i express
 const cors = require("cors"); // npm i cors
 require("dotenv").config();
+const router = express.Router();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/my_database', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to database');
+});
+
+const patientSchema = new Schema({
+  patientName: String,
+  age: Number,
+  bloodType: String,
+});
 
 // const notFoundHandler = require('./libraries/notFound');
 const unsplash = require('./libraries/unsplash');
@@ -18,6 +36,29 @@ app.use(LoggerMiddleware);
 
 
 const PORT = process.env.PORT || 3001;
+
+
+const patientModel = mongoose.model('patientModel', patientSchema);
+
+
+router.post('/my-route', (req, res) => {
+  const { patientName, age, bloodType } = req.body;
+
+  const newRecord = new patientModel({
+    patientName,
+    age,
+    bloodType,
+  });
+
+  newRecord.save((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving record');
+    } else {
+      res.status(200).send('Record saved');
+    }
+  });
+});
 
 // Routes/Endpoints
 app.get("/", homeHandler);
@@ -38,3 +79,6 @@ app.listen(PORT, () => console.log(`listening on ${PORT}`));
 module.exports = {
   app: app
 }
+
+
+module.exports = router;
